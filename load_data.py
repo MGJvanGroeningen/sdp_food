69d361b78df498fcb7bae8e737010c1c4fc97bc8
Matthijs van Groeningen <matthijsvangroeningen@gmail.com>
2023-02-20 14:08:39 +0100

    first commit

---
+++ load_data.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import os
+
+data_folder = './data'
+recipes_file = 'recipes.parquet'
+
+data = pd.read_parquet(os.path.join(data_folder, recipes_file))
+print(data.head())
